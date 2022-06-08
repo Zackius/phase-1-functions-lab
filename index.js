72de8d935a36a1da5a991977c50eb4e2470eff74
Zacharia <ndunguzachary24@gmail.com>
2022-06-08 14:55:39 +0300

    commit tree 547546c5095fd1c74b7ceb4e6b82932c109f8e77

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+ function distanceFromHqInBlocks(distance){
+return Math.abs (distance-42)
+}
+
+function distanceFromHqInFeet(distance){
+  return distanceFromHqInBlocks(distance) * 264
+
+}
+function distanceTravelledInFeet(start, destination){
+ return Math.abs(destination - start) * 264
+
+}
+
+
+function calculatesFarePrice (start, destination){
+  let actualDistance = Math.abs(destination - start) * 264
+  if(actualDistance <= 400){
+return 0
+  } else if(actualDistance > 400  &&  actualDistance <= 2000){
+
+  return (actualDistance  -  400) * 0.02
+
+} else if (actualDistance > 2000 && actualDistance <= 2500){
+  return 25
+}else{
+  return "cannot travel that far"
+}
+}
